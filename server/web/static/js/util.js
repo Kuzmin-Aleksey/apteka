da4ede6c4fe7118e1831cd8eccb93cf089e9e1e3
Алексей Кузьмин <akuzmin340@gmail.com>
2025-06-12 12:17:52 +0200

    shuffle promo

--- server/web/static/js/util.js
+++ server/web/static/js/util.js
@@ -209,3 +209,19 @@ function getNoun(number, one, two, five) {
     }
     return five;
 }
+
+function shuffle(array) {
+    let currentIndex = array.length;
+
+    // While there remain elements to shuffle...
+    while (currentIndex !== 0) {
+
+        // Pick a remaining element...
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+}
